f9de806e21de1a50c9d547320c47e72467705a4a
Caio Henrique <caiocoxim10@gmail.com>
2019-10-25 21:31:47 -0400

    teste

--- src/services/user.js
+++ src/services/user.js
@@ -2,6 +2,11 @@ import http from './config'
 
 export default {
     signup: (user) => {
+        /*const headers = {
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers' : 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+        }*/
         return http.post("/user/signup", user);
     },
     login: (email, senha) => {
