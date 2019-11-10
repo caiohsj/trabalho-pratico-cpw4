ed5946d45321d1dbbf8fd47d8c73a6b3c096f267
unknown <caiocoxim10@gmail.com>
2019-11-10 20:42:20 -0300

    Cadastro de produtos finalizado

--- src/views/ProductCreate.vue
+++ src/views/ProductCreate.vue
@@ -7,18 +7,31 @@
                 </h3>
             </div>
         </b-row>
-        <product-form></product-form>
+        <product-form v-on:save="save"></product-form>
     </div>
 </template>
 
 <script>
     import {BRow} from 'bootstrap-vue'
     import ProductForm from "../components/ProductForm";
+    import productService from '../services/product'
     export default {
         name: "ProductCreate",
         components: {
             ProductForm,
             BRow
+        },
+        methods: {
+            save(product) {
+                productService.create(product)
+                    .then((response) => {
+                        console.log(response.data)
+                        this.$router.push('/products/'+product.category.id+'/1')
+                    })
+                    .catch((response) => {
+                        console.log(response.data)
+                    })
+            }
         }
     }
 </script>
