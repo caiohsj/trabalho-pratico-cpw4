d2ad2913eade1c69f0f63bf576849f8e3ea8f84c
Caio Henrique <caiocoxim10@gmail.com>
2019-12-01 21:04:59 -0400

    dashboard implementada

---
+++ src/services/sale.js
@@ -0,0 +1,6 @@
+import http from "./config";
+export default {
+    dashboard: () => {
+        return http.get('/dashboard')
+    },
+}
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,19 +7,63 @@
                 </h3>
             </div>
         </b-row>
+        <chart v-if="loaded" v-bind:chartdata="chartData"></chart>
     </div>
 </template>
 
 <script>
     import {BRow} from 'bootstrap-vue'
+    import Chart from "../components/Chart"
+    import saleService from "../services/sale"
     export default {
         name: 'dashboard',
         components: {
+            Chart,
             BRow
         },
         data() {
             return {
-
+                chartData: {},
+                options: {responsive: true},
+                loaded: false,
+            }
+        },
+        mounted() {
+            this.chart()
+        },
+        methods: {
+            chart(){
+                saleService.dashboard()
+                    .then((response) => {
+                        this.chartData = {
+                            labels: [
+                                'Janeiro',
+                                'Fevereiro',
+                                'Março',
+                                'Abril',
+                                'Maio',
+                                'Junho',
+                                'Julho',
+                                'Agosto',
+                                'Setembro',
+                                'Outubro',
+                                'Novembro',
+                                'Dezembro'
+                            ],
+                            datasets: [
+                                {
+                                    label: 'Vendas',
+                                    backgroundColor: '',
+                                    data: response.data
+                                }
+                            ]
+                        }
+                        this.loaded = true
+                        console.log(response.data)
+                    })
+                    .catch((response) => {
+                        console.log(response.data)
+                    })
             }
         }
     }
